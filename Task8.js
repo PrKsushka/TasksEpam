a7fe40d5b10cb3015abe3fe0fbf7675f1f76cd2c
PrKsushka <ksushkaprot@gmail.com>
2021-11-07 20:19:01 +0300

    add task8

---
+++ Task8.js
@@ -0,0 +1,21 @@
+//Exeption handling
+//В try catch конструкцию завернуть код: console.log(a), let a = 3.
+// И вывести ошибку – ‘let перед использованием нужно объявить’. При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'
+
+try {
+    console.log(a);
+    let a = 3;
+} catch (e) {
+    e = new Error('let перед использованием нужно объявить');
+    console.log(e);
+}
+
+try {
+    let t = 1 / 0;
+    if (t == 'Infinity') {
+        throw 'на ноль делить нельзя';
+    }
+} catch (e) {
+    console.log(e);
+}
+
