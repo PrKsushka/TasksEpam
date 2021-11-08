6ffa4ecebecf8f40ccad000ccca5a0f8d77b348b
PrKsushka <ksushkaprot@gmail.com>
2021-11-08 17:14:43 +0300

    second version Task6

--- Task6.js
+++ Task6.js
@@ -9,7 +9,27 @@ class Cat extends Animal {
     color = 'orange';
 }
 
-let Vasya = new Cat();
-Vasya.move = false;
-console.log(Vasya);
+let vasya = new Cat();
+vasya.move = false;
+console.log(vasya);
 
+
+const animal={
+    move:true
+}
+const cat={
+    color:'orange'
+}
+cat.__proto__=animal;
+console.log(cat.move);
+
+
+class Animal2{
+
+}
+class Cat2 extends Animal2{
+    color='black'
+}
+Animal2.prototype.move=true;
+let catPetka=new Cat2();
+console.log(catPetka.move);
